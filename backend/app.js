8a0a81361f70dbaa9878205f66bcda8c1cf6d16a
atharvabhuse <atharva.bhuse@polyglots.io>
2023-04-20 01:48:36 +0530

    frontend build folder added

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,14 @@ app.use("/api/v1", user)
 app.use("/api/v1",order)
 app.use('/api/v1',payment)
 
+app.use(express.static(path.resolve(__dirname, "build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "build", "index.html"), function(err) {
+        if(err) {
+            res.status(500).send(err)
+        }
+    });
+});
 //middleware for errors
 app.use(errorMiddleware)
 
